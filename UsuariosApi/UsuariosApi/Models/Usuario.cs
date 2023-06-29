2cf9db0611ea223f149018c2e85c29fa51ab1978
Patrick Neves <you@example.com>
2023-06-28 21:04:44 -0300

    Configurando o Identity e GitIgnore

--- UsuariosApi/UsuariosApi/Models/Usuario.cs
+++ UsuariosApi/UsuariosApi/Models/Usuario.cs
@@ -1,6 +1,14 @@
-﻿namespace UsuariosApi.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UsuariosApi.Models
 {
-    public class Usuario
+    public class Usuario : IdentityUser
     {
+        public DateTime DataNascimento { get; set; }
+
+        public Usuario() : base()
+        {
+
+        }
     }
 }
