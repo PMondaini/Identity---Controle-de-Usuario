fe37e3b55788ddffe478418d7a0913dd0ceeaa26
Patrick Neves <you@example.com>
2023-07-04 21:56:46 -0300

    Criando o token para validação

---
+++ UsuariosApi/UsuariosApi/Services/TokenService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Services
+{
+    internal class TokenService
+    {
+        internal void GenerateToken(Usuario usuario)
+        {
+            Claim[] claims = new Claim[]
+            {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id),
+                new Claim(ClaimTypes.DateOfBirth, usuario.DataNascimento.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey
+                (Encoding.UTF8.GetBytes("aushdasd!@#ASD13"));
+
+            var signingCredentials =
+                new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+                (
+                    expires: DateTime.Now.AddMinutes(10),
+                    claims: claims,
+                    signingCredentials: signingCredentials
+                ); 
+
+        }
+    }
+}
